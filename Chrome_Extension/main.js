e30eb817bbce0f2e8e5c242e8d265c5ba0d16f78
Yumna Mubeen <141240511+Yumna0019@users.noreply.github.com>
2024-07-24 22:13:10 +0500

    Delete Chrome_Extension directory

--- Chrome_Extension/main.js
+++
@@ -1,8 +0,0 @@
-let myLead = []
-const inputEl = document.getElementById("input-el")
-let inputBtn = document.getElementById("input-btn")
-
-inputBtn.addEventListener("click",function(){
-    console.log("clicked");
-})
-
