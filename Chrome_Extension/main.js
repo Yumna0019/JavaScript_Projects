e55e02990e57bfb00678b722552154ccc8b945f4
Yumna <yumnamubeen0019@gmail.com>
2024-07-24 21:06:01 +0500

    first commit

---
+++ Chrome_Extension/main.js
@@ -0,0 +1,8 @@
+let myLead = []
+const inputEl = document.getElementById("input-el")
+let inputBtn = document.getElementById("input-btn")
+
+inputBtn.addEventListener("click",function(){
+    console.log("clicked");
+})
+
